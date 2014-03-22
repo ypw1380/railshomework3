362e036b319dde34f387cfd8d3973b06ce6b58be
ypw1380 <ypw1380@gmail.com>
2014-03-22 03:29:35 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActiverecordHwrk::Application.config.secret_key_base = '1e24aec1315e031eca518f23c9a5ad37d2f5b5837df5be7e595367a9dba1d0ad3aaa0ecd6aaa44a4073f0998a71db740fa1ef50eb535e3a2f181cf5cc6e31bb0'
